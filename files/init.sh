3808bf2154b71bf4cd3a980c876d376599c74e45
Minsoo Kim <leios76@gmail.com>
2015-12-17 09:29:00 +0900

    처음에 시작했을 때 기본 파일들 복사하는 내용 추가

--- files/init.sh
+++ files/init.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+if [ ! -f /redmine/config/application.rb ]
+then
+  cp -Ra /redmine.default/* /redmine/
+fi
 cd /redmine && bundle install --without development test && rake generate_secret_token && ruby bin/rails server webrick -e production -b 0.0.0.0
